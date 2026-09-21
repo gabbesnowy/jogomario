b72da1f4c041ac1c9ee385890274d38a600c52f7
gabbesnowy <gabrielafdsz2728@gmail.com>
2026-09-21 19:57:55 -0300

    envio 1

---
+++ README.md
@@ -0,0 +1,45 @@
+# Jogo Mario
+## Descrição
+Um projeto de de faculdade sobre um jogo do mario tipo o jogo do dino do chrome
+## Tecnologias
+- HTML
+- CSS
+- JavaScript
+## Instalação
+cd frontend
+npm install
+## Execução
+npm run dev
+## Integrantes
+* Ana gabriela
+
+## Integrantes
+| Nome | Matrícula | Papel |
+|----------------|-----------|---------------|
+| Ana Gabriela | 01888284 | Scrum Master |
+|  |  |  |
+|  |  |  |
+|  |  |  |
+|  |  |  |
+|  |  |  |
+|----------------|-----------|---------------|
+
+## Estrutura
+
+jogoMario/
+│
+├── backend/
+│
+├── docs/
+│ ├── branding/
+│ ├── mer/
+│ ├── mockups/
+│ ├── models/
+│ │ └── uml/
+│ └── requirements/
+│
+├── frontend/
+│
+├── .gitignore
+├── LICENSE
+└── README.md
---
+++ frontend/js/jogo.js
@@ -0,0 +1,51 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const telaGameOver = document.querySelector('.Game-over');
+
+
+function mostrarGameOver() {
+    telaGameOver.classList.add('active');
+}
+
+
+function resetGame() {
+    
+    window.location.reload(); 
+}
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+};
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens_jogo /game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        clearInterval(loop);
+
+        
+        mostrarGameOver(); 
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
